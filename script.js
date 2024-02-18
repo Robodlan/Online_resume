0db65e4ab95d1a14bcacf51bf589898f2168439d
Robodlan <reinaldo.robles@icloud.com>
2024-02-17 20:29:59 -0400

    download btn

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 window.onload = function() {
     document.getElementById("download").addEventListener("click", ()=> {
         const container = document.getElementById("container");
-        console.log(container);
-        console.log(window);
         html2pdf().from(container).save();
     })
 }
